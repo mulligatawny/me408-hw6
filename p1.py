b173c28b5c6451c083d4f6181f30406cddd08785
mulligatawny <mail.markbenjamin@gmail.com>
2021-02-23 18:55:56 -0800

    complete p1

--- p1.py
+++ p1.py
@@ -4,18 +4,21 @@ import matplotlib.pyplot as plt
 from transforms import cheby
 from scipy import integrate
 
-np.set_printoptions(precision=3)
-
 N = np.array([4, 8, 16])
-def compute_int(N):
+def compute_int(N, func=1):
     t = np.arange(0, N+1)*np.pi/N
     x = np.cos(t)
-#    f = lambda x: x**6
-#    f = lambda x: x*np.exp(-(x**2)/2) # part (a)
-    f = lambda x: np.piecewise(x, [x < 0, x >= 0], \
-    [lambda x: -2*x-1,lambda x: 2*x-1]) # part (b)
+    if func==1:
+        f = lambda x: x**6
+    elif func==2:
+        f = lambda x: x*np.exp(-(x**2)/2) # part (a)
+    else:
+        f = lambda x: np.piecewise(x, [x < 0, x >= 0], \
+        [lambda x: -2*x-1,lambda x: 2*x-1]) # part (b)
     # true integral
-    Ie = integrate.quadrature(f, -1, 1, maxiter=100)[0]
+    Ie = np.zeros_like(x)
+    for i in range(len(x)):
+        Ie[i] = integrate.quadrature(f, -1, x[i], maxiter=1000)[0]
     # chebyshev transform
     Fk = cheby.cheby(x, f)
     k = np.arange(0, N+1)
@@ -31,13 +34,13 @@ def compute_int(N):
     d[0] = sum(d[1::2]) - sum(d[::2])
     # inverse transform
     dc = cheby.icheby(t, d)
-    print(dc)
-    return Ie, dc
+    return x, Ie, dc
 
 for i in range(len(N)):
-    Ie, dc = compute_int(N[i])
-    print('For N = {}'.format(N[i]), 'the computed value is',dc[0])
-    print('The error is', (dc[0]-Ie)**2)
-    plt.plot(np.arange(N[i]+1)/N[i],dc)
-print('The true value is:', Ie)
+    x, Ie, dc = compute_int(N[i], 2)
+    plt.plot(x,dc, label='N = {}'.format(N[i]))
+plt.plot(x, Ie,'D',label='exact')
+plt.xlabel('$x$')
+plt.ylabel('Integral')
+plt.legend()
 plt.show()
